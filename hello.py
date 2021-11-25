ce3d9e83e65eeca40ec3325d3eee56ef796be0fe
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2021-11-25 21:10:23 +0600

    Some Mathematical Operations

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("Welcome to Python")
+text = "hello Python"
+print(text)
+
+# Mathematical operations
+print(2 + 2)
+number1 = 10
+number2 = 20
+result = number1 + number2
+print(result)
